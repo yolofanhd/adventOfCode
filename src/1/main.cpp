91243cb29ef6b3183e50f68cef24925fb9c5e9bf
dominik <dominik.leitgeb@htl-klu.at>
2022-12-05 12:35:15 +0100

    Added new folder structure
    Signed-off-by: dominik <dominik.leitgeb@htl-klu.at>

---
+++ src/1/main.cpp
@@ -0,0 +1,85 @@
+#include "./elf.cpp"
+#include <fstream>
+#include <iostream>
+#include <ncurses.h>
+#include <string>
+
+using namespace std;
+
+class Main1 {
+public:
+  static int startFirst() {
+    list<Elf *> elves;
+    printw("Type p or ctrl+c to quit!");
+
+    ifstream file;
+    try {
+      file.open("../assets/assets.txt");
+    } catch (exception &exp) {
+      printw("Failed to load file...");
+    }
+    string content;
+
+    // Read input file
+    if (file.is_open()) {
+      while (file) {
+        int counter = 0;
+        Elf *elf = new Elf();
+        while (getline(file, content) && content != "") {
+          ++counter;
+          elf->collect(stoi(content));
+        }
+        elves.push_back(elf);
+      }
+    }
+
+    // Finding the masterChief with most calories (O(n) solution)
+    Elf *masterChief;
+    Elf *masterChief2;
+    Elf *masterChief3;
+    list<Elf *>::iterator elf;
+    int biggestCalories = 0;
+    for (elf = elves.begin(); elf != elves.end(); ++elf) {
+      int calories = (*elf)->getTotalCalories();
+      if (biggestCalories < calories) {
+        biggestCalories = calories;
+        masterChief = *elf;
+      }
+    }
+
+    biggestCalories = 0;
+    for (elf = elves.begin(); elf != elves.end(); ++elf) {
+      int calories = (*elf)->getTotalCalories();
+      if (biggestCalories < calories &&
+          calories < masterChief->getTotalCalories()) {
+        biggestCalories = calories;
+        masterChief2 = *elf;
+      }
+    }
+
+    biggestCalories = 0;
+    for (elf = elves.begin(); elf != elves.end(); ++elf) {
+      int calories = (*elf)->getTotalCalories();
+      if (biggestCalories < calories &&
+          calories < masterChief2->getTotalCalories()) {
+        biggestCalories = calories;
+        masterChief3 = *elf;
+      }
+    }
+
+    // Printing the elf
+    char ch;
+    do {
+      masterChief->draw(1);
+      masterChief2->draw(2);
+      masterChief3->draw(3);
+      int sum = masterChief->getTotalCalories() +
+                masterChief2->getTotalCalories() +
+                masterChief3->getTotalCalories();
+      printw(to_string(sum).c_str());
+    } while ((ch = getch()) != 'q');
+
+    endwin();
+    return 0;
+  }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "./elf.cpp"
+#include "./1/main.cpp"
 #include <ncurses.h>
 #include <iostream>
 #include <fstream>
@@ -10,48 +10,8 @@ int main(int argc, char *argv[]) {
   initscr();
   cbreak();
   refresh();
-  list<Elf *> elves;
   printw("Type p or ctrl+c to quit!");
-
-
-  ifstream file;
-  file.open("../assets/assets.txt");
-  string content;
-
-  if (file.is_open()) {
-    while (file) {
-      int counter = 0;
-      Elf* elf = new Elf();
-      while (getline(file, content) && content != "") {
-        ++counter;
-        printw(content.c_str());
-        elf->collect(stoi(content));
-        printw(" ");
-      }
-      elves.push_back(elf);
-      printw("\nNEW ELF\n");
-    }
-  }
-
-  //Finding the masterChief with most calories (O(n) solution)
-  Elf *masterChief;
-  list<Elf *>::iterator elf;
-  int biggestCalories = 0;
-  for (elf = elves.begin(); elf != elves.end(); ++elf) {
-    int calories = (*elf)->getTotalCalories();
-    if (biggestCalories < calories) {
-      biggestCalories = calories;
-      masterChief = *elf;
-    }
-  }
-
-  //Printing the elf
-  char ch;
-  do{
-
-    masterChief->draw(1);
-  } while ((ch = getch()) != 'q');
-
+  Main1().startFirst();
   endwin();
   return 0;
 }
